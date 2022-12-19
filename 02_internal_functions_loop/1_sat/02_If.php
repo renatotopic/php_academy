196036320ad8af7aff492cf608108681893d1200
renatotopic <renato.topic@gmail.com>
2022-12-19 23:22:18 +0100

    if-else

---
+++ 02_internal_functions_loop/1_sat/02_If.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+var_dump(true);
+var_dump(false);
+echo '<hr>';
+
+// true
+if (true) {
+    echo 'true';
+}
+echo '<hr>';
+
+// false
+if (false) {
+    echo 'false';
+}
+echo '<hr>';
+
+$i = 3;
+// Loose equal operator - equality (value comparison)
+if ($i == 3) {
+    echo ' i equals 3 ';
+}
+echo '<hr>';
+
+var_dump($i == 3);
+
+// Strict equal operator - identity (both value and data type comparison - important)
+if ($i === 3) {
+    echo ' i equals 3 ';
+}
+echo '<hr>';
+
+// Note the data type of '3' in this comparison
+if ($i === '3') {
+    echo ' i equals 3 ';
+}
+echo '<hr>';
+var_dump($i === '3');
+echo '<hr>';
+
+// string compare
+$letter = 'cde';
+
+if ($letter == 'cde') {
+    echo 'right';
+}
+echo '<hr>';
+
+// IF - ELSE - example
+
+$i = 15;
+
+if ($i === 3) {
+    echo 'OK';
+} else {
+    // else - other cases
+    echo 'not defined number';
+}
+
+
+
+
+?>
+    
+</body>
+</html>
